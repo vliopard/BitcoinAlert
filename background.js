d9a0804e4523e646dc1d21f7a81a760a1864f607
Vincent Liopard. <vliopard@biucsproject.org>
2015-04-26 23:13:28 -0300

    Bugfix for broken javascript
    toFixed function was called by a non float variable and it was causing
    an error.

--- background.js
+++ background.js
@@ -3,10 +3,10 @@ var timer = 1;
 var percent = 1;
 var dtime = 0;
 
-var lowPrice = 90000000;
-var savedPrice = 0;
-var upperbound = 0;
-var lowerbound = 0;
+var lowPrice = 90000000.00;
+var savedPrice = 0.00;
+var upperbound = 0.00;
+var lowerbound = 0.00;
 var tempPrice = 0.00;
 
 var id = 0;
@@ -63,7 +63,7 @@ function checkCourses() {
 		var aux = elem.substring(sin);
 		var a = aux.indexOf(":");
 		var b = aux.indexOf(".");
-		tempPrice = aux.substring(a+1,b+3);
+		tempPrice = parseFloat(aux.substring(a+1,b+3));
 			var currentdate = new Date(); 
 			
 			dtime = ('0' + currentdate.getDate()).slice(-2) + '/'
@@ -79,27 +79,30 @@ function checkCourses() {
             lowPrice = parseFloat(priceL);
     });
 		var dPrice = 0.00;
+		var fPrice = 0.00;
 		if(parseFloat(tempPrice) < parseFloat(lowPrice))
 		{
-			alert("R$ "+lowPrice+" > R$ "+tempPrice+" "+dtime);
+			alert("R$ "+lowPrice.toFixed(2)+" > R$ "+tempPrice.toFixed(2)+" "+dtime);
 			dPrice = parseFloat(lowPrice) - parseFloat(tempPrice);
+			fPrice = parseFloat(tempPrice) - parseFloat(savedPrice);
 			chrome.browserAction.setIcon({path:"icon_alert.png"});
-			showNotification(tempPrice, "down", dPrice);
+			showNotification(tempPrice, "down", dPrice, fPrice);
 			chrome.storage.local.set({
 				'bitcoinalert_lprice': tempPrice});
 		}
 		else
 		{
 			dPrice = parseFloat(tempPrice) - parseFloat(lowPrice);
+			fPrice = parseFloat(savedPrice) - parseFloat(tempPrice);
 		}
 		if (savedPrice !== 0) {
 			upperbound = (parseInt(savedPrice) + (savedPrice * (percent / 100)));
 			lowerbound = (savedPrice - (savedPrice * (percent / 100)));
 			if (tempPrice.toFixed(2) > upperbound.toFixed(2)) {
-				showNotification(tempPrice, "up", dPrice);
+				showNotification(tempPrice, "up", dPrice, fPrice);
 			}
 			if (tempPrice.toFixed(2) < lowerbound.toFixed(2)) {
-				showNotification(tempPrice, "down", dPrice);
+				showNotification(tempPrice, "down", dPrice, fPrice);
 			}
 		}		
 		savedPrice = tempPrice;
@@ -107,6 +110,8 @@ function checkCourses() {
 				'bitcoinalert_price': tempPrice});
 		chrome.storage.local.set({
 				'bitcoinalert_dtime': dtime});
+		chrome.storage.local.set({
+				'bitcoinalert_delta': fPrice});
 	  }
 	}
 	xhr.send();				
@@ -118,14 +123,14 @@ function startTimer() {
 function callback() {
 }
 
-function showNotification(price, direction, change) {
+function showNotification(price, direction, change, delt) {
     var icon = "icon_up.png";
     if (direction === "down")
         icon = "icon_down.png";
     var opt = {
         type: "basic",
-        title: "Price " + direction + " " + currency + " " + change.toFixed(2),
-        message: "B 1.00 = " + currency + " " + price + " ["+ dtime +"]",
+        title: "Price " + direction + " " + currency + " " + change.toFixed(2) + " /\\ " + delt.toFixed(2),
+        message: "B 1.00 = " + currency + " " + price.toFixed(2) + " ["+ dtime +"]",
         iconUrl: icon
     }
     chrome.notifications.create(id.toString(), opt, callback);
--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 var lowPrice = 90000000;
-var savedPrice = 0;
-var currentPrice = 0;
+var savedPrice = 0.00;
+var currentPrice = 0.00;
 var currency = 0;
 var dtime = 0;
 var id = 0;
@@ -21,7 +21,7 @@ $(document).ready(function() {
     chrome.storage.local.get("bitcoinalert_lprice", function(fetchedData) {
         var priceL = fetchedData.bitcoinalert_lprice;
         if (priceL)
-            lowPrice = priceL;        
+            lowPrice = parseFloat(priceL);
     });
 
     setTimeout(function() {
@@ -43,9 +43,9 @@ $(document).ready(function() {
                 + ('0' + (currentdate.getMinutes())).slice(-2) + ":" 
                 + ('0' + (currentdate.getSeconds())).slice(-2);
 				
-			currentPrice = aux.substring(a+1,b+3);
+			currentPrice = parseFloat(aux.substring(a+1,b+3));
 			chrome.browserAction.setIcon({path:"icon.png"});
-			$("#current").text("B 1.00 = " + currency + " " + currentPrice + " (R$ " + lowPrice+ ") " + dtime);
+			$("#current").text("B 1.00 = " + currency + " " + currentPrice.toFixed(2) + " (R$ " + lowPrice.toFixed(2)+ ") " + dtime);
 			
 			if ( parseFloat(currentPrice) < parseFloat(lowPrice) )
 			{
@@ -67,6 +67,9 @@ function callback() {
 
 function notify_me() {
 
+	var delt = 0.00;
+	var change = 0.00;
+
     chrome.storage.local.get("bitcoinalert_currency", function(fetchedData) {
         var currencyO = fetchedData.bitcoinalert_currency;
         if (currencyO)
@@ -85,8 +88,15 @@ function notify_me() {
             lowPrice = priceL;        
     });
 	
+    chrome.storage.local.get("bitcoinalert_delta", function(fetchedData) {
+        var delta = fetchedData.bitcoinalert_delta;
+        if (delta)
+            delt = delta;
+    });
+
 	var direction = "stable";
     var icon = "icon_up.png";
+	
     if (savedPrice < lowPrice)
 	{
         icon = "icon_down.png";
@@ -101,8 +111,8 @@ function notify_me() {
 		
     var opt = {
         type: "basic",
-        title: "Price " + direction + " " + currency + " " + change.toFixed(2),
-        message: "B 1.00 = " + currency + " " + savedPrice + " ["+ dtime +"]",
+		title: "Price " + direction + " " + currency + " " + change.toFixed(2) + " /\\ " + delt.toFixed(2),
+        message: "B 1.00 = " + currency + " " + savedPrice.toFixed(2) + " ["+ dtime +"]",
         iconUrl: icon
     }
     chrome.notifications.create(id.toString(), opt, callback);
